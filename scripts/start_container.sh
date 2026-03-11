ab9c2bfa6051d1488d3d0450398c27e67d821b1e
Aarti-Rathore275 <rathoreaarti465@gmail.com>
2026-03-12 04:06:14 +0530

    initial commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,4 @@
 docker stop php-app || true
 docker rm php-app || true
 
-docker pull 202533526587.dkr.ecr.us-east-1.amazonaws.com/php-docker-app:latest
-
-docker run -d -p 80:80 --name php-app 202533526587.dkr.ecr.us-east-1.amazonaws.com/php-docker-app:latest
+docker run -d -p 80:80 http://202533526587.dkr.ecr.us-east-1.amazonaws.com/php-docker-app
